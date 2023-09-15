09a22250766ec0bb9378ad6fee252d62f75c9d58
Mehdi Raheemun <mraheemu@student.42.fr>
2023-09-15 12:25:46 +0200

    wip searchBar

--- src/app.js
+++ src/app.js
@@ -5,3 +5,25 @@ import IndexController from "./controllers/IndexController";
 
 const controller = new IndexController();
 console.log(controller.greet());
+
+// SearchBar Test
+
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInput = document.querySelector(".search-input");
+  const clearIcon = document.querySelector(".clear-icon");
+
+  // Afficher l'icône "clear" lorsque du texte est entré
+  searchInput.addEventListener("input", function () {
+    if (this.value) {
+      clearIcon.classList.remove("hidden");
+    } else {
+      clearIcon.classList.add("hidden");
+    }
+  });
+
+  // Effacer le texte de recherche lorsque l'icône "clear" est cliquée
+  clearIcon.addEventListener("click", function () {
+    searchInput.value = "";
+    clearIcon.classList.add("hidden");
+  });
+});
