de75850ccf82a5e52f917ac676d31ca13bd17831
Mehdi Raheemun <mraheemu@student.42.fr>
2023-09-19 19:45:51 +0200

    TagsView

---
+++ src/views/tagsView.js
@@ -0,0 +1,27 @@
+class TagsView {
+  constructor() {
+    this.tagContainer = document.querySelector(".tag-container");
+  }
+
+  createTag(type, value, removeCallback) {
+    const tagHTML = `
+        <div class="tag-view inline-flex items-center h-[53px] p-3 bg-yellow-400" data-type="${type}" data-value="${value}">
+          <span class="tag-content">${value}</span>
+          <span class="tag-close space-x-2 cursor-pointer">X</span>
+        </div>
+      `;
+
+    this.tagContainer.insertAdjacentHTML("beforeend", tagHTML);
+
+    // Ecouteur d'événement pour la suppression d'un tag
+    const tagElement = this.tagContainer.lastElementChild;
+    const tagCloseBtn = tagElement.querySelector(".tag-close");
+
+    tagCloseBtn.addEventListener("click", () => {
+      removeCallback(type, value);
+      tagElement.remove();
+    });
+  }
+}
+
+export default TagsView;
