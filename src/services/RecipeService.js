b910ee8584dc62b6c849890d9b1bf3c2508cb43d
Mehdi Raheemun <mraheemu@student.42.fr>
2023-09-19 14:38:16 +0200

    mise a jour algo selon match

--- src/services/RecipeService.js
+++ src/services/RecipeService.js
@@ -8,9 +8,22 @@ class RecipeService {
   }
 
   search(query) {
-    let result = this.allRecipes.filter((recipe) =>
-      recipe.description.includes(query)
-    );
+    let result = this.allRecipes.filter((recipe) => {
+      // Recherche dans le titre de la recette
+      const nameMatch = recipe.name.toLowerCase().includes(query.toLowerCase());
+
+      // Recherche dans la description de la recette
+      const descriptionMatch = recipe.description
+        .toLowerCase()
+        .includes(query.toLowerCase());
+
+      // Recherche dans la liste des ingrédients de la recette
+      const ingredientMatch = recipe.ingredients.some((ingredient) =>
+        ingredient.ingredient.toLowerCase().includes(query.toLowerCase())
+      );
+
+      return nameMatch || descriptionMatch || ingredientMatch;
+    });
 
     const ingredientsSet = new Set();
     const appliancesSet = new Set();
