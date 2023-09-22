aa69e94dc96489741b5a7d13790fb8ac79e4f027
Mehdi Raheemun <mraheemu@student.42.fr>
2023-09-22 12:42:56 +0200

    suppression class tailwind dans css

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
-// import "../public/styles.css";
-// import RecipeModel from "./models/RecipeModel";
-// import RecipeService from "./services/RecipeService";
-// import SearchController from "./controllers/SearchController";
-
 const model = new RecipeModel();
 const recipeService = new RecipeService();
 const controller = new SearchController(model, recipeService);
